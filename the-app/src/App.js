3f971681dc07f4e8f15b5c4ddbd2297b1ad3cd40
e-n-eliseev <e.n.eliseev@mail.ru>
2022-09-25 12:08:10 +0300

    smth

--- the-app/src/App.js
+++ the-app/src/App.js
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import './App.scss';
 import { Header } from "./components/header";
 import { Footer } from "./components/footer";
-=======
-import logo from './logo.svg';
-=======
->>>>>>> 22fa51bb3ec408f5bbcd259af3723911cdc9b443
-import './App.css';
-import {Header} from "./components/header";
-import {Footer} from "./components/footer";
->>>>>>> 453c41e19240899af63b39ee023f1ec104a5b7b3
 
 function App() {
   return (
