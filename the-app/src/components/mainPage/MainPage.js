9cd3651b56981aca2bbc60fe8c4416cfec050e85
Anton Ryabtsev <anton_ryabcev@mail.ru>
2022-10-02 15:32:41 +0300

    deleted some not actual code

--- the-app/src/components/mainPage/MainPage.js
+++ the-app/src/components/mainPage/MainPage.js
@@ -5,43 +5,6 @@ import uniqid from "uniqid";
 import { useSelector } from "react-redux";
 import { randomBooks } from "./randomBooks";
 
-
-// const bookImage = "https://source.unsplash.com/random";
-
-// const books = [
-//     {
-//         title: "Title_1",
-//         author: "Author 1",
-//         img: bookImage
-//     },
-//     {
-//         title: "Title_2",
-//         author: "Author 2",
-//         img: bookImage
-//     },
-//     {
-//         title: "Title_3",
-//         author: "Author 3",
-//         img: bookImage
-//     },
-//     {
-//         title: "Title_4",
-//         author: "Author 4",
-//         img: bookImage
-//     },
-//     {
-//         title: "Title_5",
-//         author: "Author 5",
-//         img: bookImage
-//     },
-//     {
-//         title: "Title_6",
-//         author: "Author 6",
-//         img: bookImage
-//     }
-// ];
-
-
 const MainPage = ({ authed }) => {
 
     let books = useSelector((state) => state.books).books;
