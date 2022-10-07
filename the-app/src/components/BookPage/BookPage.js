d67506db577143696285017935225f2fafa7858b
Anton Ryabtsev <anton_ryabcev@mail.ru>
2022-10-08 01:27:31 +0300

    added BookPage

--- the-app/src/components/BookPage/BookPage.js
+++ the-app/src/components/BookPage/BookPage.js
@@ -1,10 +1,27 @@
+import { useParams } from "react-router-dom";
 import "./BookPage.style.scss";
 
-export const BookPage = ({ book }) => {
-    //
+import { randomBooks } from "../mainPage/randomBooks";
+
+export const BookPage = () => {
+    
+    const params = useParams();
+    const book = randomBooks[0];
+    const { title, categories, authors } = book.volumeInfo;
+    const description = book.searchInfo.textSnippet
+
     return (
         <main className='main'>
-            <h1>BookPage</h1>
+            <div className="book__wrapper">
+                <img className="book__img" src={book.volumeInfo.imageLinks.thumbnail} alt="BookImage" />
+            </div>
+
+            <figcaption className="book__description" >
+                <h3 className="book__heading">{title ? title : "Нет информации"}</h3>
+                <p className="book__text">{description ? description : "Нет информации"}</p>
+                <p className="book__text">Категория: {categories ? categories : "Нет информации"}</p>
+                <p className="book__text">Авторы: {authors ? authors : "Нет информации"}</p>
+            </figcaption>
         </main>
     )
 }
