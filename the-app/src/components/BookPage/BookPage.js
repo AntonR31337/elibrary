78c48c1638186f6ea455e1fca51e782ea2ccc131
Anton Ryabtsev <anton_ryabcev@mail.ru>
2022-10-08 23:14:37 +0300

    сделал поиск книги в массиве книгг по ИД, добавил кнопки и иконки

--- the-app/src/components/BookPage/BookPage.js
+++ the-app/src/components/BookPage/BookPage.js
@@ -3,10 +3,14 @@ import "./BookPage.style.scss";
 
 import { randomBooks } from "../mainPage/randomBooks";
 
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import BasicRating from '../UI components/BasicRating';
+import BasicButton from "../UI components/BasicButton";
+
 export const BookPage = () => {
-    
+
     const params = useParams();
-    const book = randomBooks[0];
+    const book = randomBooks.find(el => el.id === params.id);
     const { title, categories, authors } = book.volumeInfo;
     const description = book.searchInfo.textSnippet
 
@@ -21,6 +25,22 @@ export const BookPage = () => {
                 <p className="book__text">{description ? description : "Нет информации"}</p>
                 <p className="book__text">Категория: {categories ? categories : "Нет информации"}</p>
                 <p className="book__text">Авторы: {authors ? authors : "Нет информации"}</p>
+                <div>
+                    <BasicRating />
+                </div>
+                <div className="book__buttons">
+                    <BasicButton textBtn={"В ИЗБРАННОЕ"} >
+                        <FavoriteIcon sx={{
+                            marginRight: "10px",
+                            stroke: "#1976d2",
+                            color: "white" 
+                        }} />
+                    </BasicButton>
+                    <BasicButton textBtn={"ЧИТАТЬ"} />
+                    <BasicButton textBtn={"СКАЧАТЬ"} />
+                    <BasicButton textBtn={"КУПИТЬ"} />
+                </div>
+                <p className="book__text">{description ? description : "Нет информации"}</p>
             </figcaption>
         </main>
     )
