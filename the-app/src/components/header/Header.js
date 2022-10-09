627bfe4b41a961481d8dd3e659bd66748d299b76
e-n-eliseev <e.n.eliseev@mail.ru>
2022-10-09 23:39:25 +0300

    smth

--- the-app/src/components/header/Header.js
+++ the-app/src/components/header/Header.js
@@ -1,18 +1,9 @@
 import "./header.scss";
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
 import LoginIcon from '@mui/icons-material/Login';
 import LogoutIcon from '@mui/icons-material/Logout';
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
@@ -22,7 +13,6 @@ import { Link } from "react-router-dom";
 import { logOut } from '../../firebase/firebaseConfig';
 
 import SearchForm from "../UI components/SearchForm";
-import { pages, settings } from "../../helpers/vars";
 import logo from "../../assets/bookLogo.svg";
 
 export function Header({ authed }) {
