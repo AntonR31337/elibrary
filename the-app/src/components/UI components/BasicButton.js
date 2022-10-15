e56bc20a7e0d7ff445e39f4c9052d1d79ab6437a
Anton Ryabtsev <anton_ryabcev@mail.ru>
2022-10-15 09:46:43 +0300

    сделал дизайблед кнопкок если пользователь не авторизован

--- the-app/src/components/UI components/BasicButton.js
+++ the-app/src/components/UI components/BasicButton.js
@@ -2,10 +2,11 @@ import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 
-export default function BasicButton({ textBtn, children }) {
+export default function BasicButton({ textBtn, authed = true, children }) {
+
   return (
     <Stack spacing={2} direction="row">
-      <Button variant="text">{ children }{ textBtn }</Button>
+      <Button variant="text" disabled={!authed} >{ children }{ textBtn }</Button>
       {/* <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button> */}
     </Stack>
