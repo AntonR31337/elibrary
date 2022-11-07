76761cefa246ce13cb6c521dd5d25bbcd7063ff6
Aleksey074 <94966508+Aleksey074@users.noreply.github.com>
2022-11-08 00:06:30 +0500

    favouritesPage-fix

--- the-app/src/components/favouritesPage/FavouritesPage.js
+++ the-app/src/components/favouritesPage/FavouritesPage.js
@@ -8,19 +8,41 @@ const favouritesPage = () => {
         <section className="favouritesPage-content">
 
             <div className='favouritesPage-header'>
-
-                <div className="favouritesPage-title">
-                    Избранное
-                </div>
-
-                <div className="favouritesPage-add-new">
-                    <Button ><p className='favouritesPage-add-btn'>Хочу добавить!</p><AddBoxIcon sx={{ fontSize: 35 }} /></Button>
+                <div className="favouritesPage-title-main">
+                    Мои книги
                 </div>
             </div>
 
-            <div className='favouritesPage-books'>
-                Здесь пока ничего нет...
+            <div className="favouritesPage-wish books-list">
+                <div className="favouritesPage-title-mini">
+                    Буду читать
+                </div>
+
+                <div className='wish-content books-content'>
+                    Здесь пока ничего нет...
+                </div>
             </div>
+
+            <div className="favouritesPage-continue books-list">
+                <div className="favouritesPage-title-mini">
+                    Продолжить чтение
+                </div>
+
+                <div className='continue-content books-content'>
+                    Здесь пока ничего нет...
+                </div>
+            </div>
+
+            <div className="favouritesPage-shopping books-list">
+                <div className="favouritesPage-title-mini">
+                    Покупки
+                </div>
+
+                <div className='shopping-content books-content'>
+                    Список покупок пуст
+                </div>
+            </div>
+
         </section>
     )
 };
