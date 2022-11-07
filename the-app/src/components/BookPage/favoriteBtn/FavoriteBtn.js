d7d5d58714d4957fe4cdf04e7aea99537b158870
e-n-eliseev <e.n.eliseev@mail.ru>
2022-11-08 01:01:51 +0300

    state of Icon

--- the-app/src/components/BookPage/favoriteBtn/FavoriteBtn.js
+++ the-app/src/components/BookPage/favoriteBtn/FavoriteBtn.js
@@ -3,7 +3,7 @@ import BasicButton from "../../UI components/BasicButton";
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import { doc, getDoc, setDoc } from "firebase/firestore";
 import { auth, db } from "../../../firebase/firebase";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 const FavoriteBtn = ({ book, authed }) => {
@@ -11,6 +11,7 @@ const FavoriteBtn = ({ book, authed }) => {
     let userId;
 
     const [isFavorite, setIsFavorite] = useState(false)
+    const [data, setData] = useState([])
 
     const { id } = book;
     const { title } = book.volumeInfo;
@@ -22,32 +23,51 @@ const FavoriteBtn = ({ book, authed }) => {
         thumbnail
     }
 
-
     try {
         userId = auth.currentUser.uid;
         docRef = doc(db, "users", `${userId}`);
     } catch (error) {
         console.log(error)
     }
-    const addToFavorites = async () => {
-        try {
-            const userData = await getDoc(docRef)
-            const favorites = userData._document.data.value.mapValue.fields.favoritesList.arrayValue.values
-            const currentList = favorites.map(item => {
-                const { id, title, thumbnail } = item.mapValue.fields;
-                const number = id.stringValue;
-                const name = title.stringValue;
-                const img = thumbnail.stringValue;
-                return { id: number, title: name, thumbnail: img }
-            })
-            const isInTheList = currentList.findIndex(item => item.id === currentBook.id) === -1;
+    useEffect(() => {
+        const getInfo = async () => {
+            try {
+                const userData = await getDoc(docRef)
+                const favorites = userData._document.data.value.mapValue.fields.favoritesList.arrayValue.values
+                const currentList = favorites.map(item => {
+                    const { id, title, thumbnail } = item.mapValue.fields;
+                    const number = id.stringValue;
+                    const name = title.stringValue;
+                    const img = thumbnail.stringValue;
+                    return { id: number, title: name, thumbnail: img }
+                })
+                setData(currentList)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        getInfo()
+    }, [])
+
+    useEffect(() => {
+        if (data.length) {
+            console.log(currentBook.id, data)
+            console.log(data.findIndex(item => item.id === currentBook.id))
+            const isInTheList = data.findIndex(item => item.id === currentBook.id) === -1;
             !isInTheList ? setIsFavorite(true) : setIsFavorite(false)
-            const newFavorites = isInTheList
-                ? [...currentList, currentBook]
-                : currentList.filter(item => item.id !== currentBook.id)
+        }
+    }, [data])
+
+    const addToFavorites = async () => {
+        console.log(userId, isFavorite, data)
+        try {
+            const newFavorites = !isFavorite
+                ? [...data, currentBook]
+                : data.filter(item => item.id !== currentBook.id)
             await setDoc(docRef, { favoritesList: newFavorites }, { merge: true });
             const newUserData = await getDoc(docRef)
             console.dir(newUserData._document.data.value.mapValue.fields.favoritesList.arrayValue.values);
+            setIsFavorite(!isFavorite)
         } catch (error) {
             console.log("No such document!", error);
             await setDoc(docRef, { favoritesList: [currentBook] }, { merge: true });
