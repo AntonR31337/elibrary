16b711a7bd4b7e1fa5d235ae1d21ffddf32e416c
e-n-eliseev <e.n.eliseev@mail.ru>
2022-11-28 21:48:03 +0300

    smth

--- the-app/src/components/UI components/BasicModal.js
+++ the-app/src/components/UI components/BasicModal.js
@@ -13,7 +13,7 @@ const style = {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 500,
+    width: 365,
     bgcolor: 'background.paper',
     border: '3px solid #FFCA42',
     boxShadow: 24,
